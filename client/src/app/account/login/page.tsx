7b29b38706abeb2d819eafb03048cc5726e651c0
Vladyslav Geyna <vladgeina@gmail.com>
2024-01-04 12:35:12 +0200

    Make register and login pages available only for not authorized users

--- client/src/app/account/login/page.tsx
+++ client/src/app/account/login/page.tsx
@@ -1,6 +1,7 @@
 import LoginForm from '@/components/login-form/LoginForm'
 import Title from '@/components/ui/Title'
 import WithGoogle from '@/components/with-google/WithGoogle'
+import WithoutAuth from '@/hoc/WithoutAuth'
 import { Metadata } from 'next'
 import Link from 'next/link'
 
@@ -11,23 +12,25 @@ export const metadata: Metadata = {
 
 const Login = () => {
 	return (
-		<div>
-			<Title className='text-center mt-5'>Authorization</Title>
-			<LoginForm />
-			<hr className='max-w-lg mx-auto mt-7' />
-			<p className='mt-7 text-center text-lg'>
-				Don't have account yet?{' '}
-				<Link
-					className='font-bold hover:underline'
-					href={'/account/register'}>
-					Sign up
-				</Link>
-			</p>
-			<div className='text-center mt-3'>OR</div>
-			<div className='flex justify-center mt-3'>
-				<WithGoogle text='Sign in with Google' />
+		<WithoutAuth>
+			<div>
+				<Title className='text-center mt-5'>Authorization</Title>
+				<LoginForm />
+				<hr className='max-w-lg mx-auto mt-7' />
+				<p className='mt-7 text-center text-lg'>
+					Don't have account yet?{' '}
+					<Link
+						className='font-bold hover:underline'
+						href={'/account/register'}>
+						Sign up
+					</Link>
+				</p>
+				<div className='text-center mt-3'>OR</div>
+				<div className='flex justify-center mt-3'>
+					<WithGoogle text='Sign in with Google' />
+				</div>
 			</div>
-		</div>
+		</WithoutAuth>
 	)
 }
 
--- client/src/app/account/register/page.tsx
+++ client/src/app/account/register/page.tsx
@@ -1,6 +1,7 @@
 import RegistrationForm from '@/components/registration-form/RegistrationForm'
 import Title from '@/components/ui/Title'
 import WithGoogle from '@/components/with-google/WithGoogle'
+import WithoutAuth from '@/hoc/WithoutAuth'
 import { Metadata } from 'next'
 import Link from 'next/link'
 
@@ -11,23 +12,25 @@ export const metadata: Metadata = {
 
 const Register = () => {
 	return (
-		<div>
-			<Title className='text-center mt-5'>Register</Title>
-			<RegistrationForm />
-			<hr className='max-w-lg mx-auto mt-7' />
-			<p className='mt-7 text-center text-lg'>
-				Already registered?{' '}
-				<Link
-					className='font-bold hover:underline'
-					href={'/account/login'}>
-					Sign in
-				</Link>
-			</p>
-			<div className='text-center mt-3'>OR</div>
-			<div className='flex justify-center mt-3'>
-				<WithGoogle text='Sign up with Google' />
+		<WithoutAuth>
+			<div>
+				<Title className='text-center mt-5'>Register</Title>
+				<RegistrationForm />
+				<hr className='max-w-lg mx-auto mt-7' />
+				<p className='mt-7 text-center text-lg'>
+					Already registered?{' '}
+					<Link
+						className='font-bold hover:underline'
+						href={'/account/login'}>
+						Sign in
+					</Link>
+				</p>
+				<div className='text-center mt-3'>OR</div>
+				<div className='flex justify-center mt-3'>
+					<WithGoogle text='Sign up with Google' />
+				</div>
 			</div>
-		</div>
+		</WithoutAuth>
 	)
 }
 
