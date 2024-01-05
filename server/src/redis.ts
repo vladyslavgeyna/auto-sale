4f02332c369e1200ba7e7d3ff0ea9beee1e5caaf
Vladyslav Geyna <vladgeina@gmail.com>
2024-01-05 15:56:48 +0200

    Add redis

---
+++ server/src/redis.ts
@@ -0,0 +1,56 @@
+import { Redis as IORedis } from 'ioredis'
+
+/**
+ * Redis singleton class to create a single connection to Redis
+ */
+class Redis {
+	private static instance: Redis
+	public redis: IORedis
+
+	private constructor() {
+		this.redis = new IORedis()
+	}
+
+	public static getInstance(): Redis {
+		if (!Redis.instance) {
+			Redis.instance = new Redis()
+		}
+		return Redis.instance
+	}
+}
+
+/**
+ * RedisClient class to perform Redis operations. Under the hood, it uses the Redis singleton class
+ */
+class RedisClient {
+	private redis: IORedis
+
+	constructor() {
+		this.redis = Redis.getInstance().redis
+	}
+
+	public async getString(key: string): Promise<string | null> {
+		const data = await this.redis.get(key)
+		return data
+	}
+
+	public async get<T>(key: string): Promise<T | null> {
+		const data = await this.redis.get(key)
+		return data ? (JSON.parse(data) as T) : null
+	}
+
+	public async setString(key: string, value: string, expire: number = 60) {
+		await this.redis.set(key, value, 'EX', expire)
+	}
+
+	public async set<T>(key: string, value: T, expire: number = 60) {
+		const data = JSON.stringify(value)
+		await this.redis.set(key, data, 'EX', expire)
+	}
+
+	public async delete(key: string) {
+		await this.redis.del(key)
+	}
+}
+
+export default new RedisClient()
