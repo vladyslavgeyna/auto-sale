899f0c48ecc8c4d834215bff3941891210d783d5
Vladyslav Geyna <vladgeina@gmail.com>
2024-01-30 14:58:28 +0200

    Change google user type checking. Add custom type

---
+++ server/src/resources/account/dtos/google-login.dto.ts
@@ -0,0 +1,23 @@
+/**
+ * this is user data type that we get from google
+ */
+export default interface GoogleLoginDto {
+	id: string
+	displayName: string
+	name: { familyName: string; givenName: string }
+	emails: { value: string; verified: boolean }[]
+	photos: { value: string }[]
+	provider: string
+	_raw: string
+	_json: {
+		sub: string
+		name: string
+		given_name: string
+		family_name: string
+		picture: string
+		email: string
+		email_verified: boolean
+		locale: string
+		hd: string
+	}
+}
