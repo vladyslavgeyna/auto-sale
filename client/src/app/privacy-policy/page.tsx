32022d9a39d0ec0a46cccb21117b2fbe806335d2
Vladyslav Geyna <vladgeina@gmail.com>
2024-03-02 15:54:32 +0200

    Update privacy and policy page

--- client/src/app/privacy-policy/page.tsx
+++ client/src/app/privacy-policy/page.tsx
@@ -1,63 +1,236 @@
-import Title from '@/components/ui/Title'
+import Title from "@/components/ui/Title";
 
 const page = () => {
-	return (
-		<div className='[&_p]:text-lg [&_p]:mt-3'>
-			<Title>Privacy Policy</Title>
-			<p className='!text-xl'>Last Updated: January 10, 2024</p>
-			<p>
-				This is a study project, and it is important to note that this
-				website/application is part of an educational endeavor / a
-				personal project ("pet project"). As such, it is crucial to
-				understand that this platform is primarily developed for
-				learning purposes, and no one involved in this project assumes
-				any responsibility for the information presented. Usage of this
-				platform is at your own risk.. As such, no one involved in this
-				project assumes any responsibility for the information
-				presented, and the use of this platform is at your own risk.
-			</p>
+  return (
+    <div className="[&_p]:text-lg [&_p]:mt-3">
+      <Title>Privacy Policy</Title>
+      <p className="!text-xl">Last Updated: March 2, 2024</p>
+      <p>
+        This Privacy Policy describes Our policies and procedures on the
+        collection, use and disclosure of Your information when You use the
+        Service and tells You about Your privacy rights and how the law protects
+        You.
+      </p>
+      <h2 className="text-xl font-bold mt-3">
+        Collecting and Using Your Personal Data
+      </h2>
+      <p>
+        While using Our Service, We may ask You to provide Us with certain
+        personally identifiable information that can be used to contact or
+        identify You. Personally identifiable information may include, but is
+        not limited to:
+      </p>
+      <ul>
+        <li className="text-lg">- Email address</li>
+        <li className="text-lg">- First name and last name</li>
+        <li className="text-lg">- Phone number</li>
+        <li className="text-lg">- Images</li>
+        <li className="text-lg">- Passwords</li>
+        <li className="text-lg">- Usage data</li>
+      </ul>
+      <h2 className="text-xl font-bold mt-3">Usage data</h2>
 
-			<p>
-				Please be aware that since this is a learning project,there may
-				be limitations in terms of security and data protection. We
-				recommend using fictitious or minimal personal information when
-				interacting with this platform.
-			</p>
-			<p>
-				Please note that, with the exception of the photos you upload,
-				which are stored on AWS S3, no data provided on this site is
-				shared with any third party.
-			</p>
-			<p>
-				We use cookies to enhance your experience while using this
-				platform. Cookies are small pieces of data stored on your device
-				that help us improve functionality and analyze usage patterns.
-				By using this platform, you consent to the use of cookies.
-			</p>
-			<p>
-				Please be aware that the source code for this project is
-				open-source and can be accessed on our{' '}
-				<a
-					className='text-primary hover:underline font-bold'
-					target='_blank'
-					href='https://github.com/vladyslavgeyna/auto-sale'>
-					public repository
-				</a>
-				. By using this platform, you acknowledge that the code is
-				publicly available.
-			</p>
-			<p>
-				If you have any questions or concerns, feel free to reach out to
-				us using the provided email{' '}
-				<a
-					className='text-primary hover:underline font-bold'
-					href='mailto:vladgeina@gmail.com'>
-					vladgeina@gmail.com
-				</a>
-				.
-			</p>
-		</div>
-	)
-}
+      <p>Usage Data is collected automatically when using the Service.</p>
+      <p>
+        Usage Data may include information such as Your Device's Internet
+        Protocol address (e.g. IP address), browser type, browser version, the
+        pages of our Service that You visit, the time and date of Your visit,
+        the time spent on those pages, unique device identifiers and other
+        diagnostic data.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Other data</h2>
+      <p>
+        Since our platform provides services for the posting of advertisements
+        for the sale of cars, this also includes the collection of information
+        such as:
+      </p>
+      <ul>
+        <li className="text-lg">- characteristics of the car</li>
+        <li className="text-lg">- car mileage</li>
+        <li className="text-lg">- location where this car is located</li>
+        <li className="text-lg">- car images</li>
+        <li className="text-lg">- ad title and text</li>
+        <li className="text-lg">- car price</li>
+      </ul>
 
-export default page
+      <h2 className="text-xl font-bold mt-3">
+        Information from Third-Party Social Media Services
+      </h2>
+      <p>
+        The Company allows You to create an account and log in to use the
+        Service through the following Third-party Social Media Services:
+      </p>
+      <ul>
+        <li className="text-lg">- Google</li>
+      </ul>
+      <p>
+        If You decide to register through or otherwise grant us access to a
+        Third-Party Social Media Service, We may collect Personal data that is
+        already associated with Your Third-Party Social Media Service's account,
+        such as Your name, Your email address, Your activities or Your contact
+        list associated with that account.
+      </p>
+      <p>
+        You may also have the option of sharing additional information with the
+        Company through Your Third-Party Social Media Service's account. If You
+        choose to provide such information and Personal Data, during
+        registration or otherwise, You are giving the Company permission to use,
+        share, and store it in a manner consistent with this Privacy Policy.
+      </p>
+      <h2 className="text-xl font-bold mt-3">
+        Tracking Technologies and Cookies
+      </h2>
+      <p>
+        We use Cookies and similar tracking technologies to track the activity
+        on Our Service and store information about you. Tracking technologies
+        used are beacons, tags, and scripts to collect and track information and
+        to improve and analyze Our Service. The technologies We use may include:
+      </p>
+      <ul>
+        <li className="text-lg">
+          - <strong>Cookies or Browser Cookies.</strong> A cookie is a small
+          file placed on Your Device. You can instruct Your browser to refuse
+          all Cookies or to indicate when a Cookie is being sent. However, if
+          You do not accept Cookies, You may not be able to use some parts of
+          our Service. Unless you have adjusted Your browser setting so that it
+          will refuse Cookies, our Service may use Cookies.
+        </li>
+      </ul>
+      <p>
+        Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies
+        remain on Your personal computer or mobile device when You go offline,
+        while Session Cookies are deleted as soon as You close Your web browser.
+      </p>
+      <ul>
+        <li className="text-lg">
+          - <strong>Local Storage.</strong> Local Storage is a method for web
+          applications to store data within a user's browser. It operates
+          similarly to Cookies but with larger storage capacity and improved
+          security. We may utilize Local Storage to store certain types of
+          information on Your Device, enhancing Your experience and improving
+          Our Service.
+        </li>
+      </ul>
+      <h2 className="text-xl font-bold mt-3">Use of Your Personal Data</h2>
+      <p>The Company may use Personal Data for the following purposes:</p>
+      <ul>
+        <li className="text-lg">
+          - To provide and maintain our Service, including to monitor the usage
+          of our Service.
+        </li>
+        <li className="text-lg">
+          - To manage Your Account: to manage Your registration as a user of the
+          Service. The Personal Data You provide can give You access to
+          different functionalities of the Service that are available to You as
+          a registered user.
+        </li>
+        <li className="text-lg">
+          - To contact You: To contact You by email, telephone calls, SMS, or
+          other equivalent forms of electronic communication, such as a mobile
+          application's push notifications regarding updates or informative
+          communications related to the functionalities, products or contracted
+          services, including the security updates, when necessary or reasonable
+          for their implementation.
+        </li>
+      </ul>
+      <p>We may share Your personal information in the following situations:</p>
+      <ul>
+        <li className="text-lg">
+          - With other users: when You share personal information or otherwise
+          interact in the public areas with other users, such information may be
+          viewed by all users and may be publicly distributed outside. If You
+          interact with other users or register through a Third-Party Social
+          Media Service, Your contacts on the Third-Party Social Media Service
+          may see Your names, profile, pictures and other related to you data.
+          Similarly, other users will be able to view descriptions of Your
+          activity, communicate with You and view Your profile.
+        </li>
+        <li className="text-lg">
+          - With Your consent: We may disclose Your personal information for any
+          other purpose with Your consent.
+        </li>
+      </ul>
+      <h2 className="text-xl font-bold mt-3">
+        Retention of Your Personal Data
+      </h2>
+      <p>
+        The Company will retain Your Personal Data only for as long as is
+        necessary for the purposes set out in this Privacy Policy or until you
+        delete it. We will retain and use Your Personal Data to the extent
+        necessary to comply with our legal obligations (for example, if we are
+        required to retain your data to comply with applicable laws), resolve
+        disputes, and enforce our legal agreements and policies.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Transfer of Your Personal Data</h2>
+      <p>
+        Your information, including Personal Data, is processed only within the
+        company and is not transferred to third parties except:
+      </p>
+      <ul>
+        <li className="text-lg">
+          - we use AWS S3 to store images and other files that are uploaded by
+          you
+        </li>
+        <li className="text-lg">
+          - we send emails on your provided email using NodeMailer
+        </li>
+      </ul>
+      <p>
+        The Company will take all steps reasonably necessary to ensure that Your
+        data is treated securely and in accordance with this Privacy Policy and
+        no transfer of Your Personal Data will take place to an organization or
+        a country unless there are adequate controls in place including the
+        security of Your data and other personal information.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Delete Your Personal Data</h2>
+      <p>
+        You have the right to delete or request that We assist in deleting the
+        Personal Data that We have collected about You.
+      </p>
+      <p>
+        Our Service may give You the ability to delete certain information about
+        You from within the Service.
+      </p>
+      <p>
+        You may update, amend, or delete Your information at any time by signing
+        in to Your Account, if you have one, and visiting the account settings
+        section that allows you to manage Your personal information. You may
+        also contact Us to request access to, correct, or delete any personal
+        information that You have provided to Us.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Security of Your Personal Data</h2>
+
+      <p>
+        We have implemented appropriate and reasonable technical and
+        organizational security measures designed to protect the security of any
+        personal information we process. However, despite our safeguards and
+        efforts to secure your information, no electronic transmission over the
+        Internet or information storage technology can be guaranteed to be 100%
+        secure, so we cannot promise or guarantee that hackers, cybercriminals,
+        or other unauthorized third parties will not be able to defeat our
+        security and improperly collect, access, steal, or modify your
+        information. Although we will do our best to protect your personal
+        information, transmission of personal information to and from our
+        Services is at your own risk. You should only access the Services within
+        a secure environment.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Changes to this Privacy Policy</h2>
+      <p>
+        We may update Our Privacy Policy from time to time. We will notify You
+        of any changes by posting the new Privacy Policy on this page.
+      </p>
+      <h2 className="text-xl font-bold mt-3">Contact Us</h2>
+      <p>
+        If you have any questions about this Privacy Policy, You can contact us:{" "}
+        <a
+          className="text-primary hover:underline font-bold"
+          href="mailto:vladgeina@gmail.com"
+        >
+          vladgeina@gmail.com
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default page;
