79f19fb3f6751a7b1e3fe64508bf2ce77561735f
Vladyslav Geyna <vladgeina@gmail.com>
2024-09-18 15:39:38 +0300

    Add query client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.scss";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queries";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>hello</div>
+    <QueryClientProvider client={queryClient}>
+      <div>hello</div>
+    </QueryClientProvider>
   </StrictMode>
 );
